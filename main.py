c7d11a67fe301689a69c84160c90e57df004f265
ShivSoni5 <shivsonic05@gmail.com>
2017-12-28 16:25:57 +0530

    draw_pygame

--- main.py
+++ main.py
@@ -22,20 +22,81 @@ print("Run this program with '-i 1' or '--input 1', or the number  of the input 
 exit()
 
 
+pygame.init()
+
+if args.f:
+	screenWidth, screenHeight = 1024, 768
+	screen = pygame.display.set_mode((screenWidth,screenHeight),pyagame.FULLSCREEN | pygame.HWSURFACE | pygame.DOUBLEBUF)
+
+else :
+	screenWidth, screenHeight = 800, 800
+	screen = pygame.display.set_mode((screenWidth,screenHeight))
 
 
+white = (255,255,255)
+black = (0,0,0)
 
+class Circle(object):
+	def __init__(self, x, y, color, size):
+		self.x = x
+		self.y = y
+		self.color = color
+		self.size = size
 
+	def shrink(self):
+		self.size -= 3
 
+colors = [(229, 244, 227), (93, 169, 233), (0, 63, 145), (255, 255, 255), (109, 50, 109)]
+circleList = []
 
+p = pyaudio.PyAudio()
 
+clock = pygame.time.Clock()
 
+buffer_size = 4096
+#The amount of time allotted for processing is called the Buffer Size.
 
+pyaudio_format = pyaudio.paFloat32
 
+n_channels = 1
 
+samplerate = 44100
+#Sample rate is the number of samples of audio carried per second
 
+stream = p.open(format=pyaudio_format, channels=n_channels, rate=samplerate, input=True, input_device_index=args.input, frames_per_buffer=buffer_size)
 
+time.sleep(1)
 
+tolerance = 0.8
+win_s = 4096
+hop_s = buffer_size // 2
+onset = aubio.onset("default", win_s, hop_s, samplerate)
 
+q = Queue.Queue()
 
+def draw_game():
+	running = True
+	while running:
+		key = pygame.key.get_pressed()
+		
+		if key[pygame.K_q]:
+			running = False
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
 
+		if not q.empty():
+			b = q.get()
+			newCircle = Circle(random.randint(0,screenWidth), random.randint(0,random.chioice(colors),700)
+		circleList.append(newCircle)
+		
+		screen.fill(black)
+		for place,circle in enumerate(circleList):
+			if circle.size < 1:
+				circleList.pop(place)
+			else:
+				pygame.draw.circle(screen, circle.color,(circle.x,circle.y), circle.size)
+			circle.shrink()
+		
+		pygame.display.flip()
+		clock.tick(90)
